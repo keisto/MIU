d3ad0c33f6088b9211b825d2017088acc1f09cb8
keiser <keiser@fullsail.edu>
2013-05-06 20:37:16 -0600

    VFW

---
+++ _js/json.js
@@ -0,0 +1,44 @@
+//  Mold Fitness :: Project: Web App Part 4 :: By Tony Keiser 1112
+
+var json = {
+	
+	"activity1":{
+		"name": 	["Name:","Jake Lea"],
+		"pass":	    ["Password:", "abc"],
+		"gender": 	["Gender:", "Male"],
+		"weight": 	["Weight:", "150"],
+		"muscle": 	["Activity:", "Rest"],
+		"sets": 	["Number of Sets:", "0"],
+		"reps":		["Number of Reps:", "0"],
+		"cardio":	["Cardio Today:", false],
+		"time":		["Cardio Time:", "0 Minutes"],
+		"notes":	["Notes:", "Sleep tight..."],
+		"date":		["Date:", "2013-05-03"]
+	},
+		"activity2":{
+		"name": 	["Name:","Bo Brown"],
+		"pass":	    ["Password:", "123"],
+		"gender": 	["Gender:", "Male"],
+		"weight": 	["Weight:", "230"],
+		"muscle": 	["Activity:", "Chest"],
+		"sets": 	["Number of Sets:", "5"],
+		"reps":		["Number of Reps:", "10"],
+		"cardio":	["Cardio Today:", true],
+		"time":		["Cardio Time:", "10 Minutes"],
+		"notes":	["Notes:", "Go HARD or go HOME!"],
+		"date":		["Date:", "2013-05-02"]
+	},
+		"activity3":{
+		"name": 	["Name:","Pagi Ando"],
+		"pass":	    ["Password:", "zxy"],
+		"gender": 	["Gender:", "Female"],
+		"weight": 	["Weight:", "105"],
+		"muscle": 	["Activity:", "Legs"],
+		"sets": 	["Number of Sets:", "3"],
+		"reps":		["Number of Reps:", "10"],
+		"cardio":	["Cardio Today:", true],
+		"time":		["Cardio Time:", "30 Minutes"],
+		"notes":	["Notes:", "Summer TIME!"],
+		"date":		["Date:", "2013-05-04"]
+	}		
+}
