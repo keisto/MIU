82fd26d5fd5f52c8d6a250aa78844c29904c330b
keiser <keiser@fullsail.edu>
2013-05-23 21:31:20 -0600

    Project 3

---
+++ Gold/js/script.js
@@ -0,0 +1,56 @@
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});	
+
+$('#addItem').on('pageinit', function(){
+
+		var myForm = $('#workoutForm');
+			errorslist = $('#errorslist');
+		    myForm.validate({
+			invalidHandler: function(form, validator) {
+				errorslist.click();
+				var html = '';
+				for(var key in validator.submitted){
+					var label = $('label[for^="'+ key +'"]').not('[generated]');
+					//Testing : console.log(label.text());
+					var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+					var fieldName = legend.length ? legend.text() : label.text();
+
+					html += '<li>' + fieldName + '</li>';
+					
+				};
+				$('#errors ul').html(html);
+			},
+			submitHandler: function() {
+				var data = myForm.serializeArray();
+				storeData(data);
+		}
+	});
+	
+	//any other code needed for addItem page goes here
+	
+});
+
+//The functions below can go inside or outside the pageinit function for the page in which it is needed.
+
+var autofillData = function (){
+	 
+};
+
+var getData = function(){
+
+};
+
+var storeData = function(data){
+	var id       = Math.floor(Math.random()*1000000000);
+	localStorage.setItem(id , JSON.stringify(data));
+}; 
+
+var	deleteItem = function (){
+			
+};
+					
+var clearLocal = function(){
+
+};
+
